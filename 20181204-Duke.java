453f2914ac4d2924ee0fc4290d799fdc38dd2781
liulonghaohao <45375735+liulonghaohao@users.noreply.github.com>
2018-12-20 10:34:23 +0800

    修改

--- 20181204-Duke.java
+++ 20181204-Duke.java
@@ -2,25 +2,22 @@
 //String and Class
    public class HiddenWord{
    private String puzzle;
-   public HiddenWord(){
+   public HiddenWord(String puzzle){
+      this.puzzle = puzzle;
 }
-   public String getHind(String hint,String hiddenword){
-   String c=new String(hint.length());
-   for(i=0;i<hint.length();i++){
-   if(puzzle.substring(i,i+1).compareto(hint.substring(i,i+1))!=-1){
-   c.substring(i,i+1)=hint.substring(i,i+1);
-}
-      if(puzzle.substring(i,i+1).compareto(hiddenword)=-1){
-   hint.substring(i,i+1)="-";
-}
-   if(puzzle.substring(i,i+1).compareto(hint.compareTo(puzzle)!=-1){
-   hint.substring(i,i+1)="+";
-}
+   public String getHind(String guess){
+      String hint = "";
+      for(int i = 0; i < puzzle.length(); i++){
+         if(puzzle.substring(i,i+1).equals(guess.substring(i,i+1))){
+            hint = hint+puzzle.substring(i,i+1);
+         }
+         else if(puzzle.indexOf(guess.substring(i,i+1))!=-1){
+            hint= hint + "+";
+         }
+         else{
+            hint+= "*";
+         }
+      }
+      return hint;
    }
-   return c;
-
-
-
-
-
-}
+         
